974c4cf9ab10245ce875218eb170f669e4d59cd6
pomiska <miska99@live.fi>
2023-04-28 22:53:21 +0300

    codecov ei päivitä, test commit

--- src/test/java/datastructureproject/SiirtoGeneraattoriTest.java
+++ src/test/java/datastructureproject/SiirtoGeneraattoriTest.java
@@ -6,7 +6,7 @@ import java.util.*;
 
 import org.junit.*;
 import com.github.bhlangonijr.chesslib.*; // Käytetään valmista kirjastoa siirtojen testaamisessa, koska tiedetään varmasti että se generoi kaikki lailliset siirrot
-import com.github.bhlangonijr.chesslib.move.*;
+import com.github.bhlangonijr.chesslib.move.*; //
 
 import chess.model.Side;
 
